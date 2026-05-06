5f75aba45b0b9c4d1e78dbbc75fdbd6b17f9c124
ST10473808@vcconnect.edu.za <ST10473808@vcconnect.edu.za>
2026-05-06 22:09:38 +0200

    So far this is the code for my textinput fields, dropdown, with a picture

--- App.tsx
+++ App.tsx
@@ -1,14 +1,70 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView, TextInput, } from 'react-native';
+import React from 'react';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 export default function App() {
+  // State for the text input
+  const [ dishName, setDishName ] = React.useState('');
+  const [ description, setDescription ] = React.useState('');
+  const [ price, setPrice ] = React.useState('');
+
+
+  // State for the dropdown picker
+  const [open, setOpen] = React.useState(false);
+  const [value, setValue] = React.useState(null);
+  const [items, setItems] = React.useState([
+    {label: 'Starters', value: 'starters'},
+    {label: 'Main Course', value: 'main_course'},
+    {label: 'Desserts', value: 'desserts'},
+  ]);
   return (
+    // ScrollView allows the content to be scrollable,
+    <ScrollView>
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Chef Cristofels menu</Text>
+
+      <Image source={require('./assets/food.jpg')}
+       style={{ width: 200, height: 200 }} />
       <StatusBar style="auto" />
+
+      <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1, width: 200, marginTop: 20 }}
+        onChangeText={text => setDishName(text)}
+        value={dishName}
+        placeholder="Dish name"
+      />
+
+      <DropDownPicker style={styles.dropdownContainer}
+        open={open}
+        value={value}
+        items={items}
+        setOpen={setOpen}
+        setValue={setValue}
+        setItems={setItems}
+        listMode='SCROLLVIEW'
+      />
+
+       <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1, width: 200, marginTop: 20 }}
+        onChangeText={text => setDescription(text)}
+        value={description}
+        placeholder="Description of the dish"
+      />
+
+       <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1, width: 200, marginTop: 20 }}
+        onChangeText={text => setPrice(text)}
+        value={price}
+        placeholder="Price of the dish"
+      />
+
     </View>
+
+    </ScrollView>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +73,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  dropdownContainer: {
+    width: 200,
+    marginTop: 20,
+    borderWidth: 1,
+    borderColor: 'gray',
+    alignItems: 'center',
+    
+  },
 });
+
+// What i used so far to help me code my app
+//https://hossein-zare.github.io/react-native-dropdown-picker-website/docs
+//https://reactnative.dev/docs
